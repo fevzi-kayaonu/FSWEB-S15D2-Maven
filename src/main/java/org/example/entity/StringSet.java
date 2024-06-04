6ed5654e15b558417c65b199512507435a9067a1
fevzi-kayaonu <fevzikayaonu35@gmail.com>
2024-06-04 12:30:39 +0300

    task completed

--- src/main/java/org/example/entity/StringSet.java
+++ src/main/java/org/example/entity/StringSet.java
@@ -6,22 +6,22 @@ import java.util.TreeSet;
 import java.util.stream.Collectors;
 
 public class StringSet {
-    // Sabit metin içeriği
+
     private static final String DEFAULT_TEXT = "Your default text here...";
 
     public static Set<String> findUniqueWords() {
-        // Sabit metni kullanarak kelimeleri bul
+
         return findUniqueWords(DEFAULT_TEXT);
     }
 
     public static Set<String> findUniqueWords(String text) {
-        // Noktalama işaretlerini temizle
+
         String cleanedText = text.replaceAll("[.,!?\"']", "");
-        // Kelimeleri ayır
+
         String[] words = cleanedText.split("\\s+");
-        // Kelimeleri Set'e ekle ve alfabetik olarak sıralanmış şekilde döndür
+
         return Arrays.stream(words)
-                .map(String::toLowerCase) // Kelimeleri küçük harfe çevir
+                .map(String::toLowerCase)
                 .collect(Collectors.toCollection(TreeSet::new));
     }
 }
